4077634ed02eecb1d55632478f3bb3fc1bda0051
natyosu3 <nachi334825@gmail.com>
2022-08-08 10:44:40 +0900

    update main

--- VFD.py
+++ VFD.py
@@ -4,7 +4,7 @@ from yt_dlp import YoutubeDL
 import PySimpleGUI as sg
 import pyperclip
 
-sg.theme('DarkTeal7')
+sg.theme('DarkTeal12')
 
 def workDL(ydl_opts, inp_url):
     with YoutubeDL(ydl_opts) as ydl:
@@ -18,17 +18,17 @@ def main():
     [sg.Text('入力待ち...', key='-CONDITION-')]
     ]
 
-    window = sg.Window('Video FastDownlorder', layout)
+    window = sg.Window('Video Fast Downlorder', layout)
 
     while True:
         event, value = window.read()
-        if event == '-PASTE_BTN-':
-            next = pyperclip.paste()
-            window['-INP_URL-'].update(next)
-        
 
         if event == sg.WIN_CLOSED:
             break
+        
+        if event == '-PASTE_BTN-':
+            next = pyperclip.paste()
+            window['-INP_URL-'].update(next)
 
         filename = value['-FILENAME-']
         inp_url = value['-INP_URL-']
@@ -47,7 +47,7 @@ def main():
                 executor.submit(workDL, ydl_opts, inp_url)
          
             window['-CONDITION-'].update('完了')
-            window.read(100)
+            window.read(500)
             window['-CONDITION-'].update('入力待ち...')
 
     window.close()
