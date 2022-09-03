7101f7d3a8a981fec1fce574ef9b09733a5f8700
natyosu3 <nachi334825@gmail.com>
2022-09-03 20:09:53 +0900

    update

--- VFD.py
+++ VFD.py
@@ -3,8 +3,14 @@ from sqlalchemy import false
 from yt_dlp import YoutubeDL
 import PySimpleGUI as sg
 import pyperclip
+from moviepy.editor import *
+import os
 
 sg.theme('DarkTeal12')
+
+def remove():
+    os.remove('video')
+    os.remove('audio')
 
 def workDL(ydl_opts, inp_url):
     with YoutubeDL(ydl_opts) as ydl:
@@ -15,6 +21,7 @@ def main():
     [sg.Text('URL'), sg.Input(key='-INP_URL-'), sg.Button('paste', key='-PASTE_BTN-')],
     [sg.Text('保存するファイル名'), sg.InputText(key='-FILENAME-')],
     [sg.Text(size=(45, 2)), sg.Button('Download', key='-DOWNLOAD-')],
+    [sg.Checkbox('mp3出力', key='-CHECK1-')],
     [sg.Text('入力待ち...', key='-CONDITION-')]
     ]
 
@@ -39,12 +46,28 @@ def main():
             window['-CONDITION-'].update('処理中')
             window.read(360)
 
-            ydl_opts = {
-                'outtmpl':path,
-            }
-            with ThreadPoolExecutor(max_workers=2) as executor:
-                executor.submit(workDL, ydl_opts, inp_url)
-         
+            if value['-CHECK1-'] == True:
+
+                ydl_opts = {
+                    'outtmpl':'video',
+                    'format': 'bestvideo',
+                }
+                with ThreadPoolExecutor(max_workers=2) as executor:
+                    executor.submit(workDL, ydl_opts, inp_url)
+
+                ydl_opts = {
+                    'outtmpl':'audio',
+                    'format': 'bestaudio',
+                }
+                with ThreadPoolExecutor(max_workers=2) as executor:
+                    executor.submit(workDL, ydl_opts, inp_url)
+
+                clip = VideoFileClip("video")
+                audioclip = AudioFileClip("audio")
+                new_videoclip = clip.set_audio(audioclip)
+                new_videoclip.write_videofile(path)
+                remove()
+
             window['-CONDITION-'].update('完了')
             window.read(1200)
             window['-CONDITION-'].update('入力待ち...')
